1eb518b87851f29807ebed914b5ab85949062ff6
Aleksander <aleksandersn@gmail.com>
2014-09-11 11:17:47 +0200

    updated MVC

--- MVC.php
+++ MVC.php
@@ -2,4 +2,9 @@
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($controller, $model);
+ 
+if (isset($_GET['action']) && !empty($_GET['action'])) {
+    $controller->{$_GET['action']}();
+}
+ 
 echo $view->output();
