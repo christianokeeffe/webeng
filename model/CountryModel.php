802f0794d94b55dbf24733012c112735129d5a47
Aleksander <aleksandersn@gmail.com>
2014-09-12 10:20:08 +0200

    added some code to replace things

--- model/CountryModel.php
+++ model/CountryModel.php
@@ -4,18 +4,20 @@ class Model
 {
     public $string;
 	public $listOfCountries;
+	private $con;
  
     public function __construct(){
         $this->getData();
-    }
-	
-	private function getData(){
 		$con=mysqli_connect("localhost","root","root","mondial");
-		
 		// Check connection
 		if (mysqli_connect_errno()) {
 		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
+		mysqli_close($con);
+    }
+	
+	private function getData(){
+		mysqli_open($con);
 
 		$result = mysqli_query($con,"SELECT * FROM country");
 		
@@ -23,11 +25,31 @@ class Model
 		$this->listOfCountries = array();
 		
 		while($row = mysqli_fetch_array($result)) {
-			$tmp = new Country($row['name'],$row['capital'],$row['population']);
+			$tmp = new Country($row['id'],$row['name'],$row['capital'],$row['population']);
 			array_push($this->listOfCountries,$tmp);
 			$counter++;
 		}
 
+		mysqli_close($con);
+	}
+	
+	public function setData($objectToChange, $objectToChangeToo){
+		if (in_array($objectToChange,$this->listOfCountries)){
+			$key = array_search($objectToChange,$this->listOfCountries);
+			$this->listOfCountries[$key]->id = $objectToChangeToo->id;
+			$this->listOfCountries[$key]->name = $objectToChangeToo->name;
+			$this->listOfCountries[$key]->capital = $objectToChangeToo->capital;
+			$this->listOfCountries[$key]->population = $objectToChangeToo->population;
+			
+			$this->dbReplace($objectToChange->id, $objectToChangeToo->id, $objectToChangeToo->name, $objectToChangeToo->capital, $objectToChangeToo->population);
+		} else {
+			echo "ERROR";
+		}
+	}
+		
+	private function dbReplace($searchKey, $id, $name, $capital, $population){
+		mysqli_open($con);
+		mysqli_query($con,"UPDATE country SET id=". $id ."AND name=". $name ."AND capital=". $capital ."AND population=". $population ."WHERE id=". $searchKey);
 		mysqli_close($con);
 	}
 }
--- model/metadata/CountryMetaData.php
+++ model/metadata/CountryMetaData.php
@@ -1,13 +1,19 @@
 <?php
 class Country{
+	private $id;
 	private $name;
 	private $capital;
 	private $population;
 	
-	public function __construct($n, $c, $p){
+	public function __construct($i, $n, $c, $p){
+		$this->id = $id;
 		$this->name = $n;
 		$this->capital = $c;
 		$this->population = $p;
+	}
+	
+	public function getId(){
+		return $this->id;
 	}
 	
 	public function getName(){
