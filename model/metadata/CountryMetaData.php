5007cbf1afb36717e862f2493a0b44297d9dbba4
Christian OKeeffe <christian@joedal.dk>
2014-09-18 12:09:42 +0200

    dummy work

--- model/metadata/CountryMetaData.php
+++ model/metadata/CountryMetaData.php
@@ -1,15 +1,39 @@
 <?php
 class Country{
-	private $id;
-	private $name;
 	private $capital;
+	private $carcode;
+	private $datacode;
+	private $gdp_agri;
+	private $gdp_ind;
+	private $gdp_serv;
+	private $gdp_total;
+	private $goverment;
+	private $id;
+	private $indep_date;
+	private $infant_mortality;
+	private $inflation;
+	private $name;
 	private $population;
+	private $population_growth;
+	private $total_area;
 	
-	public function __construct($i, $n, $c, $p){
-		$this->id = $i;
-		$this->name = $n;
-		$this->capital = $c;
-		$this->population = $p;
+	public function __construct($capital,$carcode,$datacode,$gdp_agri,$gdp_ind,$gdp_serv,$gdp_total,$goverment,$id,$indep_date,$infant_mortality,$inflation,$name,$population,$population_growth,$total_area){
+		$this->capital = $capital;
+		$this->carcode = $carcode;
+		$this->datacode = $datacode;
+		$this->gdp_agri = $gdp_agri;
+		$this->gdp_ind = $gdp_ind;
+		$this->gdp_serv = $gdp_serv;
+		$this->gdp_total = $gdp_total;
+		$this->goverment = $goverment;
+		$this->id = $id;
+		$this->indep_date = $indep_date;
+		$this->infant_mortality = $infant_mortality;
+		$this->inflation = $inflation;
+		$this->name = $name;
+		$this->population = $population;
+		$this->population_growth = $population_growth;
+		$this->total_area = $total_area;
 	}
 	
 	public function getId(){
