f8ba8a882ee8579ad5704ac45d74c60f7b41e7a9
Christian OKeeffe <christian@joedal.dk>
2014-09-19 09:29:06 +0200

    added insert to DB

--- model/ImportModel.php
+++ model/ImportModel.php
@@ -8,15 +8,32 @@ class ImportModel
         $this->getData();
     }
 	
-	public function setData($objectToChange, $objectToChangeToo){
-		if (in_array($objectToChange,$this->listOfCountries)){
-			$key = array_search($objectToChange,$this->listOfCountries);
-			$this->listOfCountries[$key] = $objectToChangeToo;
+	public function insertToDB($tableName, $listOfObjects){
+		$con = conFatory();
+		mysqli_query($con,"TRUNCATE TABLE $tableName");
+
+		switch ($tableName) {
+			case 'country':
+				$first = true;
+				$sqlstring = "INSERT INTO country (capital,carcode,datacode,gdp_agri,gdp_ind,gdp_serv,gdp_total,goverment,id,indep_date,infant_mortality,inflation,name,population,population_growth,total_area) VALUES ";
+				foreach ($listOfObjects as $country) {
+					if(!$first)
+					{
+						$sqlstring .= ",";
+					}
+					$sqlstring .= "('". $country->getCapital() . "','" . $country->getCarcode() . "','" . $country->getDatacode() . "'," . $country->getGdp_agri() . "," . $country->getGdp_ind() . "," . $country->getGdp_serv() . ",";
+					$sqlstring .= $country->getGdp_total() . ",'". $country->getGoverment() . "','" . $country->getId() . "','" . $country->getIndep_date() . "','" . $country.getInfant_mortality() . "',";
+					$sqlstring .= $country->getInflation() . ",'" . $country->getName() . "'," . $country->getPopulation() . "," . $country.getPopulation_growth() . "," . $country.getTotal_area() . ")"; 
+				}
+				$sqlstring .= ";";
+				mysqli_query($con, $sqlstring);
+				break;
 			
-			$this->dbReplace($objectToChange->getId(), $objectToChangeToo->getName(), $objectToChangeToo->getCapital(), $objectToChangeToo->getPopulation());
-		} else {
-			echo "ERROR";
+			default:
+				# code...
+				break;
 		}
+		mysqli_close($con);
 	}
 		
 	private function dbReplace($searchKey, $name, $capital, $population){
