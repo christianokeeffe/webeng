c5c234017655073e6de6df73c99242a643c2688c
Christian OKeeffe <christian@joedal.dk>
2014-09-25 12:01:27 +0200

    :hamburger:

--- api/index.php
+++ api/index.php
@@ -5,19 +5,38 @@ include("../SQLConnect.php");
 include("../model/metadata/CountryMetaData.php");
 include("../model/CountryModel.php");
 header('Content-Type: application/json');
-
+    	$f3->cmodel = new CountryModel();
 $f3->route('GET /',
     function() {
         echo 'Cool api!';
     }
 );
 $f3->route('GET /country',
-    function() {
-    	$cmodel = new CountryModel();
-        echo json_encode($cmodel->listOfCountries);
+    function($f3) {
+        echo json_encode($f3->cmodel->listOfCountries);
     }
 );
-
+$f3->route('GET /country/@id',
+    function($f3) {
+    	$cont = null;
+    	for($i = 0; $i < count($f3->cmodel->listOfCountries); $i++)
+    	{
+    		if($f3->cmodel->listOfCountries[$i]->getId() == $f3->get('PARAMS.id'))
+    		{
+    			$cont = $f3->cmodel->listOfCountries[$i];
+    			break;
+    		}
+    	}
+    	if($cont == null)
+    	{
+    		echo "Country not foundt";
+    	}
+    	else
+    	{
+        	echo json_encode($cont);
+    	}
+    }
+);
 
 
 $f3->run(); 
