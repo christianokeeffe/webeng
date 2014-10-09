ef0966724529cbed460bf6bfa5a779b5e52612de
Christian OKeeffe <christian@joedal.dk>
2014-10-09 10:17:01 +0200

    link working

--- view/topbar.php
+++ view/topbar.php
@@ -1,31 +1,31 @@
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">TACDB</a>
+      <a class="navbar-brand" href="?page=index">TACDB</a>
     </div>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-        <li class="<?echo $front?>"><a href="#">Frontpage</a></li>
+        <li class="<?echo $front?>"><a href="?page=index">Frontpage</a></li>
         <li class="dropdown <?echo $reg?>">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Regional views<span class="caret"></span></a>
+          <a href="?page=index" class="dropdown-toggle" data-toggle="dropdown">Regional views<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Continent</a></li>
-            <li><a href="#">Country</a></li>
-            <li><a href="#">Province</a></li>
-            <li><a href="#">City</a></li>
+            <li><a href="?page=index">Continent</a></li>
+            <li><a href="?page=countryList">Country</a></li>
+            <li><a href="?page=index">Province</a></li>
+            <li><a href="?page=index">City</a></li>
           </ul>
         </li>
         <li class="dropdown <?echo $stat?>">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Statistical views<span class="caret"></span></a>
+          <a href="?page=index" class="dropdown-toggle" data-toggle="dropdown">Statistical views<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Population</a></li>
-            <li><a href="#">GDP</a></li>
-            <li><a href="#">Religions</a></li>
-            <li><a href="#">Governments</a></li>
+            <li><a href="?page=index">Population</a></li>
+            <li><a href="?page=index">GDP</a></li>
+            <li><a href="?page=index">Religions</a></li>
+            <li><a href="?page=index">Governments</a></li>
           </ul>
         </li>
-        <li><a href="?page=import" class="<?echo $import?>">Import data</a></li>
-        <li><a href="#" class="<?echo $about?>">About</a></li>
+        <li class="<?echo $imp?>"><a href="?page=import" >Import data</a></li>
+        <li><a href="?page=index" class="<?echo $about?>">About</a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
 
